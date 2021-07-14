a3579d934239ad7b2b00f0a4d0adaaebba60e06e
raymondshum <rshum@csumb.edu>
2021-07-13 18:14:24 -0700

    updated partials

--- index.js
+++ index.js
@@ -8,7 +8,25 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index',{"title":"Overview"});
+});
+app.get('/about', (req, res) => {
+  res.render('about',{"title":"About"});
+});
+app.get('/contact', (req, res) => {
+  res.render('contact',{"title":"Contact"});
+});
+app.get('/economic-impact', (req, res) => {
+  res.render('economic-impact',{"title":"Economic Impact"});
+});
+app.get('/environmental-impact', (req, res) => {
+  res.render('environmental-impact',{"title":"Environmental Impact"});
+});
+app.get('/future', (req, res) => {
+  res.render('future',{"title":"Future"});
+});
+app.get('/sources', (req, res) => {
+  res.render('sources',{"title":"Sources"});
 });
 
 app.listen(3000, "127.0.0.1", () => {
